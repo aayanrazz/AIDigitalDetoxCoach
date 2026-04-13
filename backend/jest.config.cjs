5ade2d6326097d89ff4b9c21ca5e3550f1f5cdf9
aayanraj <aayanrazzdhital9@gmail.com>
2026-04-14 00:27:19 +0545

    Update jest config

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,27 +1,19 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-
   extensionsToTreatAsEsm: ['.ts'],
-
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
       {
         useESM: true,
-        tsconfig: '<rootDir>/tsconfig.test.json',
-      },
-    ],
+        tsconfig: '<rootDir>/tsconfig.test.json'
+      }
+    ]
   },
-
   testMatch: ['<rootDir>/tests/controllers/**/*.test.ts'],
-
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
   moduleFileExtensions: ['ts', 'js', 'json'],
-
   clearMocks: true,
-  restoreMocks: true,
   verbose: true,
-  testTimeout: 120000,
+  testTimeout: 60000
 };
